a892268ef7f124b9521a1546d2e869f6e6f10988
Tanzeel Ur Rhman <tanzrehman2020@gmail.com>
2026-03-31 23:43:24 +0500

    finally and custom errors

--- exception_handling_1.py
+++ exception_handling_1.py
@@ -1,18 +1,31 @@
+# try:
+#     # Taking input from user
+#     num1 = float(input("Enter first number: "))
+#     num2 = float(input("Enter second number: "))
+    
+#     # Performing division
+#     result = num1 / num2
+    
+#     print("Result:", result)
+
+# except ValueError:
+#     print("Invalid input! Please enter numbers only.")
+
+# except ZeroDivisionError:
+#     print("Cannot divide by zero!")
+
+# except Exception as e:
+#     print("Something went wrong:", e)
+
+# finally:
+#     print("Execution completed.")
 try:
-    # Taking input from user
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    num = int(input("Enter a number between 1 and 10: "))
     
-    # Performing division
-    result = num1 / num2
+    if num < 1 or num > 10:
+        raise ValueError("Number must be between 1 and 10.")
     
-    print("Result:", result)
+    print("Valid number:", num)
 
-except ValueError:
-    print("Invalid input! Please enter numbers only.")
-
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-
-except Exception as e:
-    print("Something went wrong:", e)
+except ValueError as e:
+    print("Error:", e)   
